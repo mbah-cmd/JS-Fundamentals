639e297eef3c13a8a132cd58f7b8d0c2eb50c7e1
henry mbah <chigoziembah73@gmail.com>
2025-07-10 01:42:41 -0700

    added 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,16 @@
+
+
+
+
+const args = process.argv.slice(2);
+
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else if (args.length === 2) {
+  console.log("Arguments found");
+}
+
+
